ff941fb5902d9d6a02faf788e2cebd19166875f3
Thomas Martin <thomas.j.martin@powerhrg.com>
2024-07-10 14:09:03 -0400

    Create a Anagram class and methods

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require "pry"
+class Anagram
+
+  def initialize word
+    @word =  word
+  end
+
+  def match word_list
+    word_list.select do |str|
+     str.chars.sort == @word.chars.sort
+    end
+  end
+
+end
